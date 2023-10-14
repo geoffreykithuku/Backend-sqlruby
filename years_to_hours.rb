6bb99b5343838f9d6dcf02f31490580dad451fce
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-10-14 21:15:49 +0300

    fixed ruby assignment

--- years_to_hours.rb
+++ years_to_hours.rb
@@ -1,26 +1,29 @@
 
+#years to hours
 
-def years_to_hours
-    puts "Enter a number of years"
-    years = gets.chomp 
-    years = years.to_i 
-    hours = years * 365 * 24
-    puts "That's #{hours} hours."
-end
+puts "Enter a number of years"
+years = gets.chomp 
+years = years.to_i 
+hours = years * 365 * 24
+puts "That's #{hours} hours."
 
-def decades_to_minutes
-    puts "Enter a number of decades"
-    decades = gets.chomp 
-    decades = decades.to_i 
-    minutes = decades * 10 * 365 * 24 * 60
-    puts "That's #{minutes} minutes."
-end
 
-def age_to_seconds
-    puts "Enter your age"
-    age = gets.chomp 
-    age = age.to_i 
-    seconds = age * 365 * 24 * 60 * 60
-    puts "That's #{seconds} seconds."
-end
 
+
+#decades to minutes
+
+puts "Enter a number of decades"
+decades = gets.chomp 
+decades = decades.to_i 
+minutes = decades * 10 * 365 * 24 * 60
+puts "That's #{minutes} minutes."
+
+
+
+#age to seconds
+
+puts "Enter your age"
+age = gets.chomp 
+age = age.to_i 
+seconds = age * 365 * 24 * 60 * 60
+puts "That's #{seconds} seconds."
